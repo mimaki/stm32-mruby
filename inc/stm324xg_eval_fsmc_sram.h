a40d41a95f9974eb39bb9aeaba76e9241ba71c17
mimaki <hiroshi.mimaki@gmail.com>
2013-07-28 21:04:12 +0900

    modify PWM (re-commit)

--- inc/stm324xg_eval_fsmc_sram.h
+++ inc/stm324xg_eval_fsmc_sram.h
@@ -80,8 +80,8 @@
   */      
 
 void SRAM_Init(void);
-void SRAM_WriteBuffer(uint8_t* pBuffer, uint32_t WriteAddr, uint32_t NumHalfwordToWrite);
-void SRAM_ReadBuffer(uint8_t* pBuffer, uint32_t ReadAddr, uint32_t NumHalfwordToRead);
+void SRAM_WriteBuffer(uint16_t* pBuffer, uint32_t WriteAddr, uint32_t NumHalfwordToWrite);
+void SRAM_ReadBuffer(uint16_t* pBuffer, uint32_t ReadAddr, uint32_t NumHalfwordToRead);
 
 #ifdef __cplusplus
 }
